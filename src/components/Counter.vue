cbe46b0fc1a7c7f402e45a0043d7cbd1896a7dfd
fooheeyuan <61861689+fooheeyuan@users.noreply.github.com>
2020-03-10 04:03:16 +0800

    grocery

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="headerStyle">
+    <h1>{{i}}</h1>
+    <button v-on:click="increase(1)">Try increase</button>
+    <br />
+    <br />
+    <button v-on:click="decrease(1)">Try decrease</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      i: 0
+    };
+  },
+  methods: {
+    increase: function(x) {
+      this.i = this.i + x;
+    },
+
+    decrease: function(x) {
+      this.i -= x;
+    }
+  }
+};
+</script>
---
+++ src/components/PageContent.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <counter></counter>
+    <ul>
+      <li v-for="item in itemsList" v-bind:key="item.name">
+        <h2>{{item.name}}</h2>
+        <counter></counter>
+
+        <img v-bind:src="item.image" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+import Counter from "./Counter.vue";
+export default {
+  components: {
+    counter: Counter
+  },
+
+  data() {
+    return {
+      itemsList: [
+        { name: "f1", image: "./assets/f1.jpg", show: false },
+        { name: "ion", image: "/assets/ion.jpg", show: false },
+        { name: "mbs", image: "/assets/mbs.jpg", show: false },
+        { name: "flyer", image: "/assets/flyer.jpg", show: false },
+        {
+          name: "gardensbythebay",
+          image: "/assets/gardensbythebay.jpg",
+          show: false
+        },
+        { name: "nus", image: "/assets/nus.jpg", show: false }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  background: rgb(122, 63, 100);
+  padding: 10px;
+  border-style: solid;
+  border-color: black;
+  border-width: 2px;
+}
+h1 {
+  color: ivory;
+}
+h2 {
+  text-align: center;
+}
+#itemsList {
+  width: 100%;
+  max-width: 120px;
+  margin: 30px auto;
+  padding: 0 5px;
+  box-sizing: border-box;
+}
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 10px;
+  border: 1px solid #222;
+  margin: 10px;
+}
+
+img {
+  max-width: 150px;
+}
+</style>
